4c1542c1ea483449f6b871013b1638b3ba6a1fb4
CriFigShe <crifigshe@gmail.com>
2025-08-12 17:24:08 +0200

    Create Restaurant service

---
+++ src/main/java/com/cristian/restaurant/services/impl/RestaurantServiceImpl.java
@@ -0,0 +1,50 @@
+package com.cristian.restaurant.services.impl;
+
+import com.cristian.restaurant.domain.GeoLocation;
+import com.cristian.restaurant.domain.RestaurantCreateUpdateRequest;
+import com.cristian.restaurant.domain.entities.Address;
+import com.cristian.restaurant.domain.entities.Photo;
+import com.cristian.restaurant.domain.entities.Restaurant;
+import com.cristian.restaurant.repositories.RestaurantRepository;
+import com.cristian.restaurant.services.GeoLocationService;
+import com.cristian.restaurant.services.RestaurantService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.elasticsearch.core.geo.GeoPoint;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RestaurantServiceImpl implements RestaurantService {
+
+    private final RestaurantRepository restaurantRepository;
+    private final GeoLocationService geoLocationService;
+
+    @Override
+    public Restaurant createRestaurant(RestaurantCreateUpdateRequest request) {
+        Address address = request.getAddress();
+        GeoLocation geoLocation = geoLocationService.geoLocate(address);
+        GeoPoint geoPoint = new GeoPoint(geoLocation.getLatitude(), geoLocation.getLongitude());
+
+        List<String> photoIds = request.getPhotoIds();
+        List<Photo> photos = photoIds.stream().map(photoUrl -> Photo.builder()
+                .url(photoUrl)
+                .uploadDate(LocalDateTime.now())
+                .build()).toList();
+
+        Restaurant restaurant = Restaurant.builder()
+                .name(request.getName())
+                .cuisineType(request.getCuisineType())
+                .contactInformation(request.getContactInformation())
+                .address(address)
+                .geoLocation(geoPoint)
+                .operatingHours(request.getOperatingHours())
+                .averageRating(0f)
+                .photos(photos)
+                .build();
+
+        return restaurantRepository.save(restaurant);
+    }
+}
