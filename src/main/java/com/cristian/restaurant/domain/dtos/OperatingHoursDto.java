d9e2b47f4d6902c7413e66434dd692c19f275d04
CriFigShe <crifigshe@gmail.com>
2025-08-12 17:34:40 +0200

    Create Restaurant Dto (Correct TimeRangeDto)

--- src/main/java/com/cristian/restaurant/domain/dtos/OperatingHoursDto.java
+++ src/main/java/com/cristian/restaurant/domain/dtos/OperatingHoursDto.java
@@ -1,6 +1,7 @@
 package com.cristian.restaurant.domain.dtos;
 
 import com.cristian.restaurant.domain.entities.TimeRange;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,17 +16,24 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 @Builder
 public class OperatingHoursDto {
 
+    @Valid
     private TimeRangeDto monday;
 
+    @Valid
     private TimeRangeDto tuesday;
 
+    @Valid
     private TimeRangeDto wednesday;
 
+    @Valid
     private TimeRangeDto thursday;
 
+    @Valid
     private TimeRangeDto friday;
 
+    @Valid
     private TimeRangeDto saturday;
 
+    @Valid
     private TimeRangeDto sunday;
 }
