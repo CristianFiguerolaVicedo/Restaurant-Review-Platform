373d788dfce166f2602b2f30eea7bf5a0e61ea83
CriFigShe <crifigshe@gmail.com>
2025-08-12 15:07:40 +0200

    Security config

---
+++ src/main/java/com/cristian/restaurant/repositories/RestaurantRepository.java
@@ -0,0 +1,10 @@
+package com.cristian.restaurant.repositories;
+
+import com.cristian.restaurant.domain.entities.Restaurant;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends ElasticsearchRepository<Restaurant, String> {
+
+}
