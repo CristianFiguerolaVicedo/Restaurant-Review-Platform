0938e4d1bbc7649b5cd2a417ed24a1a0f9c391e4
CriFigShe <crifigshe@gmail.com>
2025-08-12 12:26:50 +0200

    Project initialized

---
+++ src/main/java/com/cristian/restaurant/RestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.cristian.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantApplication.class, args);
+	}
+
+}
