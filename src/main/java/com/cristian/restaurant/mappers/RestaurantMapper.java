efbcb83cb0075ee6cb01f8344f4a266755b8b93f
CriFigShe <crifigshe@gmail.com>
2025-08-13 11:10:25 +0200

    Error handling

--- src/main/java/com/cristian/restaurant/mappers/RestaurantMapper.java
+++ src/main/java/com/cristian/restaurant/mappers/RestaurantMapper.java
@@ -10,13 +10,13 @@ import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 
-@Mapper(componentModel = "string", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RestaurantMapper {
     RestaurantCreateUpdateRequest toRestaurantCreateUpdateRequest(RestaurantCreateUpdateRequestDto dto);
 
     RestaurantDto toRestaurantDto(Restaurant restaurant);
 
-    @Mapping(target = "latitude", expression = "java(geoPoint.getLat()")
-    @Mapping(target = "longitude", expression = "java(geoPoint.getLon()")
+    @Mapping(target = "latitude", expression = "java(geoPoint.getLat())")
+    @Mapping(target = "longitude", expression = "java(geoPoint.getLon())")
     GeoPointDto toGeoPointDto(GeoPoint geoPoint);
 }
