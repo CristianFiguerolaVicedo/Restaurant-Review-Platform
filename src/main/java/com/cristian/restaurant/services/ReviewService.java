d3a39265de339bd0030f17b2d462a4e4d3c1ebf6
CriFigShe <crifigshe@gmail.com>
2025-08-13 15:03:32 +0200

    Review list service

--- src/main/java/com/cristian/restaurant/services/ReviewService.java
+++ src/main/java/com/cristian/restaurant/services/ReviewService.java
@@ -3,7 +3,10 @@ package com.cristian.restaurant.services;
 import com.cristian.restaurant.domain.ReviewCreateUpdateRequest;
 import com.cristian.restaurant.domain.entities.Review;
 import com.cristian.restaurant.domain.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ReviewService {
     Review createReview(User author, String restaurantId, ReviewCreateUpdateRequest review);
+    Page<Review> listReviews(String restaurantId, Pageable pageable);
 }
