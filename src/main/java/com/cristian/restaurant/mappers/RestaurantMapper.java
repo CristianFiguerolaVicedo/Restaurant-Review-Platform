e1f9f6f1a180e2e10aa7432d8305f0613e670d52
CriFigShe <crifigshe@gmail.com>
2025-08-13 11:38:57 +0200

    Restaurant search Dto and mapper

--- src/main/java/com/cristian/restaurant/mappers/RestaurantMapper.java
+++ src/main/java/com/cristian/restaurant/mappers/RestaurantMapper.java
@@ -4,17 +4,30 @@ import com.cristian.restaurant.domain.RestaurantCreateUpdateRequest;
 import com.cristian.restaurant.domain.dtos.GeoPointDto;
 import com.cristian.restaurant.domain.dtos.RestaurantCreateUpdateRequestDto;
 import com.cristian.restaurant.domain.dtos.RestaurantDto;
+import com.cristian.restaurant.domain.dtos.RestaurantSummaryDto;
 import com.cristian.restaurant.domain.entities.Restaurant;
+import com.cristian.restaurant.domain.entities.Review;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RestaurantMapper {
     RestaurantCreateUpdateRequest toRestaurantCreateUpdateRequest(RestaurantCreateUpdateRequestDto dto);
 
     RestaurantDto toRestaurantDto(Restaurant restaurant);
+
+    @Mapping(source = "reviews", target = "totalReviews", qualifiedByName = "populateTotalReviews")
+    RestaurantSummaryDto toSummaryDto(Restaurant restaurant);
+
+    @Named("populateTotalReviews")
+    private Integer populateTotalReviews(List<Review> reviews) {
+        return reviews.size();
+    }
 
     @Mapping(target = "latitude", expression = "java(geoPoint.getLat())")
     @Mapping(target = "longitude", expression = "java(geoPoint.getLon())")
